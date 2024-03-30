2a7e42768512f59928353fcc69b6e6c2fb08d97d
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-30 17:27:17 +0600

    js hw 16

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const urlAPI = 'https://freetestapi.com/api/v1/books';
-  const container = document.querySelector('.container');
-
- 
-  // const xhr = new XMLHttpRequest();
-  // xhr.open('GET', urlAPI, true);
-  // xhr.addEventListener('load', () => {
-  //   // if (xhr.status === 200) {
-  //   const data = JSON.parse(xhr.responseText); // JSON.parse = json()
-  //   renderData(data);
-  // });
-
-  // xhr.send();
-
-  function renderData(data) {
-    data.books.map((book, index) => {
-      const name = document.createElement('p');
-      const price = document.createElement('p');
-      const img = document.createElement('img');
-
-      name.innerHTML = `${index + 1} ${book.title}`;
-      price.innerHTML = book.price;
-      img.setAttribute('src', book.thumbnail);
-
-      container.appendChild(name);
-      container.appendChild(price);
-      container.appendChild(img);
-
-      console.log(data.name);
-    });
-    
-  }
-
-
-  // fetch(urlAPI)
-  //   .then(function (response) {
-  //     if (response.ok) {
-  //       return response.json();
-  //     }
-  //     throw new Error('Network response was not ok.');
-  //   })
-  //   .then(function (data) {
-  //     console.log('data', data);
-  //     renderData(data);
-  //   })
-  //   .catch(function (error) {
-  //     console.log('error', error);
-  //   });
-
-  axios
-    .get(urlAPI)
-    .then(function (response) {
-      renderData(response.data);
-      console.log('response', response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
-
+    const urlAPI = 'https://dummyjson.com/products';
+    const container = document.querySelector('.container');
+  
+    // 1 method
+    // const xhr = new XMLHttpRequest();
+    // xhr.open('GET', urlAPI, true);
+    // xhr.addEventListener('load', () => {
+    //   // if (xhr.status === 200) {
+    //   const data = JSON.parse(xhr.responseText); // JSON.parse = json()
+    //   renderData(data);
+    // });
+  
+    // xhr.send();
+  
+    function renderData(data) {
+      data.products.map((product, index) => {
+        const name = document.createElement('p');
+        const price = document.createElement('p');
+        const img = document.createElement('img');
+  
+        name.innerHTML = `${index + 1} ${product.title}`;
+        price.innerHTML = product.price;
+        img.setAttribute('src', product.thumbnail);
+  
+        container.appendChild(name);
+        container.appendChild(price);
+        container.appendChild(img);
+  
+        return container;
+      });
+    }
+  
+    // 2 method
+    // fetch(urlAPI)
+    //   .then(function (response) {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //     throw new Error('Network response was not ok.');
+    //   })
+    //   .then(function (data) {
+    //     console.log('data', data);
+    //     renderData(data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log('error', error);
+    //   });
+  
+    axios
+      .get(urlAPI)
+      .then(function (response) {
+        renderData(response.data);
+        console.log('response', response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
